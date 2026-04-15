7cfa0d49a7ce9f0552de748706936d08a0d59c0e
DFCU\stuart.smith <stuarts205@gmail.com>
2026-04-15 14:09:14 -0600

    change host

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,4 +2,4 @@ import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
 // .api to enter /api prefix
-export const client = treaty<App>('localhost:3000').api
+export const client = treaty<App>('https://realtime-chat-tan-beta.vercel.app/').api
