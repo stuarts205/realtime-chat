844128c2d319eb422049d71153d9a55f957d5a52
DFCU\stuart.smith <stuarts205@gmail.com>
2026-04-20 15:10:40 -0600

    update again

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,4 +2,4 @@ import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
 // .api to enter /api prefix
-export const client = treaty<App>('https://studepiasite.com/').api
+export const client = treaty<App>('https://stupediasite.com/').api
