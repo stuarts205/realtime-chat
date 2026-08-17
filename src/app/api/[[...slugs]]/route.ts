04e025056acf48522f472e1373b68d0e0992a5e8
DFCU\stuart.smith <stuarts205@gmail.com>
2026-08-17 15:36:57 -0600

    increase time

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "./auth";
 import { z } from "zod";
 import { Message, realtime } from "@/lib/realtime";
 
-const ROOM_TTL_SECONDS = 60 * 10;
+const ROOM_TTL_SECONDS = 60 * 30;
 
 const rooms = new Elysia({ prefix: "/room" })
   .post("/create", async () => {
